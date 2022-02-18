c6ca199a7de8d26e1558d505d2438d3ac51e1b67
m-aljamal <mohammadjamol@gmail.com>
2022-02-18 08:45:20 +0300

    create disc

--- src/components/CreateDiscount.tsx
+++ src/components/CreateDiscount.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-
-interface IDiscount {
-  absence?: Number;
-  date: Date;
-  employeeId: String;
-  late?: Number;
-  notes: String;
-  punishment?: Number;
-}
-
+import { CreateDiscountMutationVariables } from "src/generated/generates";
+import { useCreateDiscount } from "src/utils/discount";
 const CreateDiscount = () => {
-  const { register, handleSubmit } = useForm<IDiscount>();
-
-  const onSubmit = (data: IDiscount) => {
-    console.log(data);
+  const { register, handleSubmit } = useForm<CreateDiscountMutationVariables>();
+  const { mutate } = useCreateDiscount();
+  const onSubmit = (data: CreateDiscountMutationVariables) => {
+    mutate({
+      date: data.date,
+      employeeId: data.employeeId,
+      absence: data.absence ? +data.absence : null,
+      late: data.late ? +data.late : null,
+      notes: data.notes,
+      punishment: data.punishment ? +data.punishment : null,
+    });
   };
   return (
     <div>
@@ -29,19 +28,31 @@ const CreateDiscount = () => {
           {...register("absence")}
           placeholder="absence"
           className="block m-1"
+          type="number"
         />
-        <input {...register("late")} placeholder="late" className="block m-1" />
+        <input
+          {...register("late")}
+          placeholder="late"
+          className="block m-1"
+          type="number"
+        />
         <input
           {...register("punishment")}
           placeholder="punishment"
           className="block m-1"
+          type="number"
         />
         <input
           {...register("notes")}
           placeholder="notes"
           className="block m-1"
         />
-        <input {...register("date")} placeholder="date" className="block m-1" />
+        <input
+          type="date"
+          {...register("date")}
+          placeholder="date"
+          className="block m-1"
+        />
         <button className="bg-blue-400" type="submit">
           Submit
         </button>
